5f048ec321011a7c685ce78fbfd5894a8e2123c2
Giovani Augusto Justo <giovanijusto54@gmail.com>
2024-03-19 22:34:29 -0300

    Finalização 19/03

---
+++ src/main/java/br/com/aldenoir/testecrud/presentation/LoginFilter.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.aldenoir.testecrud.presentation;
+
+/**
+ *
+ * @author giovani.ajusto
+ */
+public class LoginFilter {
+    
+}
